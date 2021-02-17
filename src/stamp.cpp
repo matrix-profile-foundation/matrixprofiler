4da06350e59bbaaaced6cd4d588d33686abd418c
Francisco Bischoff <franzbischoff@gmail.com>
2021-02-17 02:57:53 +0000

    asan/ubsan ok

--- src/stamp.cpp
+++ src/stamp.cpp
@@ -22,7 +22,7 @@ List stamp_rcpp(const NumericVector data_ref, const NumericVector query_ref, uin
                 bool progress) {
 
   bool partial = false;
-  uint64_t exclusion_zone = round(window_size * ez + DBL_EPSILON);
+  int64_t exclusion_zone = round(window_size * ez + DBL_EPSILON);
   uint64_t data_size = data_ref.length();
   uint64_t query_size = query_ref.length();
   uint64_t matrix_profile_size = data_size - window_size + 1;
@@ -70,8 +70,8 @@ List stamp_rcpp(const NumericVector data_ref, const NumericVector query_ref, uin
 
       // apply exclusion zone
       if (exclusion_zone > 0) {
-        uint64_t exc_st = MAX(0, (i - exclusion_zone));
-        uint64_t exc_ed = MIN(matrix_profile_size - 1, i + exclusion_zone);
+        int64_t exc_st = MAX(0, (i - exclusion_zone));
+        int64_t exc_ed = MIN(matrix_profile_size - 1, i + exclusion_zone);
         IntegerVector dp_range = Range(exc_st, exc_ed);
         distance_profile[dp_range] = R_PosInf;
       }
